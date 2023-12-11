6a8c55fc19609cc940be32308673b6b0128ada0c
nanophyto <coralscaper@gmail.com>
2023-12-11 16:45:18 +0000

    fixed formatting and now Gamma regression

--- coccodata/regression.py
+++ coccodata/regression.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def regression_simulation(X_train, X_predict, Y_train, n):
+class regression_simulation():
     """
     A function to simulate POC estimates based on regression.
 
@@ -27,31 +27,51 @@ def regression_simulation(X_train, X_predict, Y_train, n):
     Example
     -------
     d = pd.read_csv("/home/phyto/CoccoData/data/unprocessed/poulton2024.csv")
-    Y = d['pg poc']
-    X = d['volume']
-    test1 = regression_simulation(X, 100, Y, 100)
+    Y_train = d['pg poc']
+    X_train = d['volume']
+    X_predict = [100, 99, 98, 101, 102, 100]*1000
+    m = regression_simulation(X_train, X_predict, Y_train)
+    test1 = m.simulate_data()
     sns.histplot(x=test1)
     plt.show()
     
     """
-    def regression_simulation_sample(X_train, X_predict, Y_train):
-            
-        model = sm.GLM(Y_train, X_train, family=sm.families.Gamma(sm.families.links.identity())) # sm.OLS(Y, X)
-        results = model.fit()
+    def __init__(self, X_train, X_predict, Y_train):
 
-        gen = model.get_distribution(params = results.params, scale = results.scale, exog=100)
+        self.model = sm.GLM(Y_train, X_train, family=sm.families.Gamma(sm.families.links.identity())) # sm.OLS(Y, X)
+        self.results = self.model.fit()
+        self.X_predict = X_predict
+        self.X_train = X_train
+        self.Y_train = Y_train
+    
 
-        simulated_data = gen.rvs(len(X_train))
+    def regression_simulation_sample(self, X_predict_sample):
+        
+        gen = self.model.get_distribution(params = self.results.params, scale = self.results.scale, exog=X_predict_sample)
+
+        simulated_data = gen.rvs(len(self.X_train))
+
         # simulated_data = simulated_data[simulated_data > 0] not needed for Gamma
         simulated_sample = np.random.choice(simulated_data, 1)
+
         return(simulated_sample)
 
+    def simulate_data(self):
 
-    simulated_data = []
-    for i in range(n):
-        simulated_data.extend(regression_simulation_sample(X_train, X_predict, Y_train,))
+        simulated_data = []
 
-    return(simulated_data)
+        for i in range(len(self.X_predict)):
+            simulated_data.extend(self.regression_simulation_sample(self.X_predict[i]))
 
+        return(simulated_data)
+
+# d = pd.read_csv("/home/phyto/CoccoData/data/unprocessed/poulton2024.csv")
+# Y_train = d['pg poc']
+# X_train = d['volume']
+# X_predict = [100, 99, 98, 101, 102, 100]*1000
+# m = regression_simulation(X_train, X_predict, Y_train)
+# test1 = m.simulate_data()
+# sns.histplot(x=test1)
+# plt.show()
 
 print("fin")
