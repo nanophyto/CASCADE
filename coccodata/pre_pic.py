d909ff80cd39124cd2b6cbdd2ed546341a183ea2
nanophyto <coralscaper@gmail.com>
2023-12-12 15:49:16 +0000

    preprocessing for pic

---
+++ coccodata/pre_pic.py
@@ -0,0 +1,95 @@
+import numpy as np 
+import pandas as pd
+import sys
+
+sys.path.insert(0, '/home/phyto/CoccoData/coccodata/')
+from functions import rename_synonyms
+
+def pre_devries2024():
+    def estimate_pontosphaera():
+        pic_per_lith = [77.97, 133.07, 237.68] #Guerreiro2021
+        observations_per_estimate = [13, 5, 2] #Guerreiro2021
+        number_of_liths = [45]   #yang and wei 2003
+
+        p=observations_per_estimate/np.sum(observations_per_estimate) 
+
+        pic_sphere = np.random.choice(pic_per_lith, 10000, p=p)*np.random.choice(number_of_liths, 10000)
+
+        mean_pic_per_sphere =  np.round(np.mean(pic_sphere), -2)*0.12
+        sd_pic_per_sphere = np.round(np.std(pic_sphere), -2)*0.12
+
+        d = {'species':['undefined Pontosphaera'], 
+            'mean': [mean_pic_per_sphere],
+            'sd': [sd_pic_per_sphere],
+            'ref': ['guerreiro2021 and yang2003']}
+
+        d = pd.DataFrame(d)
+
+        return(d)
+
+
+    def estimate_flabellatus():
+
+        pic_per_lith = [9.65, 11.62, 4.25] #Guerreiro2021
+        observations_per_estimate = [213, 106, 108] #Guerreiro2021
+        number_of_liths = [40, 86, 56, 18, 120]   #yang and wei 2003
+
+        p=observations_per_estimate/np.sum(observations_per_estimate) 
+
+        pic_sphere = np.random.choice(pic_per_lith, 10000, p=p)*np.random.choice(number_of_liths, 10000)
+
+        mean_pic_per_sphere =  np.round(np.mean(pic_sphere), -1)*0.12
+        sd_pic_per_sphere = np.round(np.std(pic_sphere), -1)*0.12
+
+
+        d = {'species':['Gladiolithus flabellatus'], 
+            'mean': [mean_pic_per_sphere],
+            'sd': [sd_pic_per_sphere],
+            'ref': ['guerreiro2021 and yang2003']}
+
+        d = pd.DataFrame(d)
+
+        return(d)
+
+    d_flabellatus = estimate_flabellatus()
+    d_pontosphaera = estimate_pontosphaera()
+
+    d = pd.concat([d_flabellatus, d_pontosphaera])
+
+    d.to_csv("/home/phyto/CoccoData/data/pic/devries2024.csv", index=False)
+
+
+
+def pre_sheward2024():
+
+    #read sheward size data
+    d = pd.read_csv("/home/phyto/CoccoData/data/unprocessed/pic/sheward2024_pic.csv")
+
+    d = d[d['Coccosphere condition']=="Intact"]
+    d['Species'] = d['Species'].str.strip()
+    d['Genus'] = d['Genus'].str.strip()
+    d['species'] = d['Genus'] + " " + d['Species'] 
+
+    d = d[['species', 'pg pic']]
+
+    d = rename_synonyms(d, remove_duplicate=False)
+
+    d = d.groupby(by="species").agg(["mean", "std"]).reset_index()
+    d['sd'] = np.round(d['pg pic']['std'], 1)
+    d['mean'] = np.round(d['pg pic']['mean'], 1)
+    d = d[['species', 'sd', 'mean']]
+
+    #rename because column names are tuples for some reason??
+    d.columns = ['species', 'sd', 'mean']
+
+    d['reference'] = 'sheward2024'
+    d['method'] = "AMT morphometrics"
+    d = d[['species', 'mean', 'sd', 'method', 'reference']] 
+    d.to_csv("/home/phyto/CoccoData/data/pic/sheward2024.csv", index=False)
+
+
+pre_sheward2024()
+pre_devries2024()
+print("fin")
+
+
