909b931119fd2023a6b7cec6dbe915bc2d10b19f
nanophyto <coralscaper@gmail.com>
2023-12-12 13:04:29 +0000

    fixed subplot spacing issue

--- coccodata/plotting.py
+++ coccodata/plotting.py
@@ -10,13 +10,15 @@ import numpy as np
 def multipage_plots(d, species_list, n_page, out_path):
 
     def dist_plots(ax, variable, species):
-        df = d[d['Species']==species]
-        grid = sns.histplot(df, x=variable, ax=ax)
+        df = d[d['species']==species]
+        df = df[df['variable']==variable]
+        grid = sns.histplot(df, x="value", ax=ax)
         return(grid)
 
     def sub_plots_ax(species):
         cols = 4
         rows = len(species)
+        plt.figure(figsize=(18, 28))
 
         for j in range(rows):
 
@@ -25,29 +27,45 @@ def multipage_plots(d, species_list, n_page, out_path):
             ax3 = subplot(rows,cols,(j*cols)+3)
             ax4 = subplot(rows,cols,(j*cols)+4)
 
-            dist_plots(ax=ax1, variable="diameter", species=species[j])
-            dist_plots(ax=ax2, variable="volume", species=species[j])
-            dist_plots(ax=ax3, variable="pg poc", species=species[j])
+            dist_plots(ax=ax2, variable="diameter", species=species[j])
+            dist_plots(ax=ax3, variable="volume", species=species[j])
+            dist_plots(ax=ax4, variable="pg poc", species=species[j])
             image = plt.imread("/home/phyto/SEM_eva/JRYSEM-287-07p.jpeg")
-            ax4.imshow(image)    
+            ax1.imshow(image)  
+            ax1.set_axis_off()
+            ax2.axes.get_yaxis().set_visible(False)
+            ax3.axes.get_yaxis().set_visible(False)
+            ax4.axes.get_yaxis().set_visible(False)
+
+            ax1.title.set_text(species[j])
+            ax2.title.set_text("diameter (um)")
+            ax3.title.set_text("volume (um^3)")
+            ax4.title.set_text("POC (pg C)")
+
+        plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.5)
+        
+
 
 
 
     n = n_page
-    # using list comprehension 
     final = [species_list[i * n:(i + 1) * n] for i in range((len(species_list) + n - 1) // n )]  
 
     with PdfPages(out_path) as pdf:
         for i in range(len(final)):
             sub_plots_ax(final[i])
             pdf.savefig()
+            plt.close()
 
     print("finished exporting pdf to: " + out_path)
 
+#
+# d = pd.read_csv("/home/phyto/CoccoData/data/unprocessed/poulton2024.csv")
+# species_list = ["Emiliania huxleyi", "Calcidiscus leptoporus", "Syracosphaera pulchra", "Calcidiscus leptoporus",
+#                 "Emiliania huxleyi", "Calcidiscus leptoporus", "Syracosphaera pulchra", "Calcidiscus leptoporus",
+#                 "Emiliania huxleyi", "Calcidiscus leptoporus", "Syracosphaera pulchra", "Calcidiscus leptoporus",
+#                 "Emiliania huxleyi", "Calcidiscus leptoporus", "Syracosphaera pulchra", "Calcidiscus leptoporus"]
 
-d = pd.read_csv("/home/phyto/CoccoData/data/unprocessed/poulton2024.csv")
-species_list = ["Emiliania huxleyi", "Calcidiscus leptoporus", "Syracosphaera pulchra", "Emiliania huxleyi", "Calcidiscus leptoporus", "Syracosphaera pulchra"]
+# multipage_plots(d=d, species_list=species_list, n_page=8, out_path='/home/phyto/multipage_pdf.pdf')
 
-multipage_plots(d=d, species_list=species_list, n_page=6, out_path='/home/phyto/multipage_pdf.pdf')
-
-print("fin")
+#print("fin")
