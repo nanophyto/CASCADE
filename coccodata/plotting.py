db3926fd4cbdb4ce0b032c63fbe9ec7f47903b69
nanophyto <coralscaper@gmail.com>
2024-01-30 15:00:22 +0000

    function now plots lat-lon and depth-time

--- coccodata/plotting.py
+++ coccodata/plotting.py
@@ -5,6 +5,11 @@ import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 from pylab import *
 import numpy as np
+import scipy.stats as st 
+import cartopy.crs as ccrs
+import cartopy as cart
+from matplotlib import cm
+
 
 from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
@@ -14,83 +19,88 @@ rc('text', usetex=True)
 
 class multipage_plots():
 
-    def __init__(self, d, species_list, n_page, out_path):
-        self.d = d
+    def __init__(self, ds, species_list, n_page, out_path):
         self.species_list = species_list
         self.n = n_page
         self.out_path = out_path
+        self.ds = ds
 
-    def dist_plots(self, ax, variable, species, log_scale=False):
-        df = self.d[self.d['species']==species]
-        poc = df[df['variable']=='pg poc']['value']
-        pic = df[df['variable']=='pg pic']['value']
-        df = df[df['variable']==variable]
+    def latlon_plots(self, species, ax=None, title=None, log_scale=False, add_colorbar=False):
 
+        #ax = plt.axes(projection=ccrs.PlateCarree())
+        d = self.ds[self.ds[species]>0]
+
+        d = d.groupby(['Latitude', 'Longitude'])[species].agg('mean').reset_index()
+
+        ax.coastlines()
+        ax.add_feature(cart.feature.LAND, zorder=1, edgecolor='k', facecolor="gray")
+        ax.gridlines(draw_labels=True, zorder=1)
         
-        try:
-            # if percentile is not None:
-            #     d = d[d['value']> np.percentile(df['value'], 100-percentile)]
-            #     d = d[d['value']< np.percentile(df['value'], percentile)]
-            x = np.random.choice(df['value'], 1000)
-            #x = d['value']
-        except:
-            x = []
 
-        #grid = sns.histplot(x = x,ax=ax, kde = True, stat="density")
-        grid = sns.kdeplot(x = x, ax=ax, log_scale=log_scale)
+        grid = sns.scatterplot(x=d['Longitude'], y=d['Latitude'], ax=ax, hue=np.log(d[species]), 
+                               palette='viridis', edgecolor = "none") # marker="$\circ$",  ec="face", alpha=0.5
+       
+        #sm = plt.cm.ScalarMappable(cmap="viridis")
+        ax.get_legend().remove()    
+        #ax.figure.colorbar(sm)
+
+        # plt.scatter(, 
+        #             s=100,
+        #             alpha=0.75,
+        #             transform=ccrs.PlateCarree()) ## Important
+
+        ax.set_ylim(-90, 90)
+        ax.set_xlim(-180, 180)
 
 
-        if variable == 'pg poc':
-            #pic_poc = np.random.choice(pic, 1000)/np.random.choice(poc, 1000)
-
-
-            pic_median = np.round(np.median(pic), 1)
-            poc_median = np.round(np.median(poc), 1)
-            #pic_poc_median = np.round(np.median(pic_poc), 1)
-
-            pic_ci = np.round(np.std(pic), 1)
-            poc_ci = np.round(np.std(poc), 1)
-            #pic_poc_ci = np.round(np.std(pic_poc), 1)
-
-            # Build a rectangle in axes coords
-
-            grid.text(0.95, 0.95, r'POC:' +  str(poc_median) + '± ' + str(poc_ci),
-                horizontalalignment='right',
-                verticalalignment='top',
-                transform=grid.transAxes)
-            grid.text(0.95, 0.85, r'PIC:' +  str(pic_median) + '± ' + str(pic_ci),
-                horizontalalignment='right',
-                verticalalignment='top',
-                transform=grid.transAxes)        
-            #grid.text(0.95, 0.75, r'PIC/POC:' +  str(pic_poc_median) + '± ' + str(pic_poc_ci),
-            #    horizontalalignment='right',
-            #    verticalalignment='top',
-            #    transform=grid.transAxes)   
-            
-            p = plt.Rectangle((0, 0), 1, 1, fill=False)
-            p.set_transform(grid.transAxes)
-            p.set_clip_on(False)
-            grid.add_patch(p)
+        #cax = fig.add_axes([ax.get_position().x1+0.01,ax.get_position().y0,0.02,ax.get_position().height])#
+        if title==None:
+            ax.set_title(species)
         else:
-            None
-        return(grid)
+            ax.set_title(title)
+
+    def depthtime_plots(self, species, ax=None, title=None, log_scale=False, add_colorbar=False):
+
+        #ax = plt.axes(projection=ccrs.PlateCarree())
+        
+        d = self.ds[self.ds[species]>0]
+
+        d = d.groupby(['Depth', 'months since winter solstice'])[species].agg('mean').reset_index()
+
+
+        grid = sns.scatterplot(x=d['months since winter solstice'], y=d['Depth'], palette='viridis', 
+                               hue=np.log10(d[species]), ax=ax, edgecolor = "none") #marker="$\circ$",  ec="face", 
+        sm = plt.cm.ScalarMappable(cmap="viridis")
+        ax.get_legend().remove()
+        
+    
+        ax.figure.colorbar(sm)
+
+
+        ax.set_ylim(300, 0)
+        ax.set_xlim(1, 12)
+
+        #cax = fig.add_axes([ax.get_position().x1+0.01,ax.get_position().y0,0.02,ax.get_position().height])#
+        if title==None:
+            ax.set_title(species)
+        else:
+            ax.set_title(title)
+
 
     def sub_plots_ax(self, species):
-        cols = 4
+        cols = 3
         rows = len(species)
         plt.figure(figsize=(18, 28))
         species = list(species)
         for j in range(rows):
 
             ax1 = subplot(rows,cols,(j*cols)+1)
-            ax2 = subplot(rows,cols,(j*cols)+2)
+            ax2 = subplot(rows,cols,(j*cols)+2, projection=ccrs.PlateCarree())
             ax3 = subplot(rows,cols,(j*cols)+3)
-            ax4 = subplot(rows,cols,(j*cols)+4)
 
-            self.dist_plots(ax=ax2, variable="diameter", species=species[j])
-            self.dist_plots(ax=ax3, variable="pg poc", species=species[j], log_scale=True)
-            self.dist_plots(ax=ax3, variable="pg pic", species=species[j], log_scale=True)
-            self.dist_plots(ax=ax4, variable="pg pic", species=species[j], log_scale=True)
+            self.latlon_plots(ax=ax2, species=species[j])
+            self.depthtime_plots(ax=ax3, species=species[j])
+
             try:
                 image = plt.imread("/home/phyto/CoccoData/data/sem/" + species[j] + ".jpeg")
             except:
@@ -98,27 +108,26 @@ class multipage_plots():
             ax1.imshow(image)  
             ax1.set_axis_off()
             ax2.axes.get_yaxis().set_visible(False)
-            ax3.axes.get_yaxis().set_visible(False)
-            ax4.axes.get_yaxis().set_visible(False)
-            species_title = r'\textbf{\textit{' + str(species[j]) + '}}'
-            ax1.title.set_text(species_title)
-            ax2_title = r'\textbf{Diameter (um)'
 
+
+            species_title =  str(species[j]) 
+            ax1.title.set_text(species_title)
+
+            ax2_title = r'\textbf{Diameter (um)'
             ax2.title.set_text(ax2_title )
             
             ax3_title = r'\textbf{Particulate Carbon (pg C)}'
             ax3.title.set_text(ax3_title)
             
-            ax4.title.set_text("PIC (pg C)")
 
         plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=1)
-        
+    
 
 
     def export_pdf(self):
 
         n = self.n
-        final = [species_list[i * n:(i + 1) * n] for i in range((len(self.species_list) + n - 1) // n )]  
+        final = [self.species_list[i * n:(i + 1) * n] for i in range((len(self.species_list) + n - 1) // n )]  
 
         with PdfPages(self.out_path) as pdf:
             for i in range(len(final)):
@@ -130,17 +139,35 @@ class multipage_plots():
         print("finished exporting pdf to: " + self.out_path)
 
 
+d = pd.read_csv("/home/phyto/CoccoData/gridded_abundances.csv")
+d['months since winter solstice'] = d['Month']
+d.loc[(d['Latitude'] <0) & (d['Month'] == 1),'months since winter solstice']= 7
+d.loc[(d['Latitude'] <0) & (d['Month'] == 2),'months since winter solstice']= 8
+d.loc[(d['Latitude'] <0) & (d['Month'] == 3),'months since winter solstice']= 9
+d.loc[(d['Latitude'] <0) & (d['Month'] == 4),'months since winter solstice']= 10
+d.loc[(d['Latitude'] <0) & (d['Month'] == 5),'months since winter solstice']= 11
+d.loc[(d['Latitude'] <0) & (d['Month'] == 6),'months since winter solstice']= 12
+d.loc[(d['Latitude'] <0) & (d['Month'] == 7),'months since winter solstice']= 1
+d.loc[(d['Latitude'] <0) & (d['Month'] == 8),'months since winter solstice']= 2
+d.loc[(d['Latitude'] <0) & (d['Month'] == 9),'months since winter solstice']= 3
+d.loc[(d['Latitude'] <0) & (d['Month'] == 10),'months since winter solstice']= 4
+d.loc[(d['Latitude'] <0) & (d['Month'] == 11),'months since winter solstice']= 5
+d.loc[(d['Latitude'] <0) & (d['Month'] == 12),'months since winter solstice']= 6
 
 
-d = pd.read_csv("/home/phyto/CoccoData/data/species_list.csv")
-species_list = d['species']
-d= pd.read_csv("/home/phyto/CoccoData/fml2.csv")
-#m = multipage_plots(d=d, species_list=species_list[0:7], n_page=8, out_path='/home/phyto/library.pdf')
-m = multipage_plots(d=d, species_list=species_list, n_page=6, out_path='/home/phyto/library.pdf')
+ds = pd.read_csv("/home/phyto/CoccoData/data/species_list.csv")
+ds = ds.sort_values(by=['species'])
+#species_list = list(ds['species'])[0:6]
+species_list = list(ds['species'])
 
+# d = pd.read_csv("/home/phyto/CoccoData/final_pic.csv")
+
+# # #m = multipage_plots(d=d, species_list=species_list[0:7], n_page=8, out_path='/home/phyto/library.pdf')
+
+m = multipage_plots(ds=d, species_list=species_list, n_page=6, out_path='/home/phyto/library.pdf')
 m.export_pdf()
 
-print("fin")#
+# print("fin")
 
 # species_list = ["Emiliania huxleyi", "Calcidiscus leptoporus", "Syracosphaera pulchra", "Calcidiscus leptoporus",
 #                 "Emiliania huxleyi", "Calcidiscus leptoporus", "Syracosphaera pulchra", "Calcidiscus leptoporus",
