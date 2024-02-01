f1e263af111e0b15481d83d8c7fa73d39fd373dd
nanophyto <coralscaper@gmail.com>
2024-02-01 16:12:21 +0000

    attempt to fix issue with dot product shape

--- coccodata/regression.py
+++ coccodata/regression.py
@@ -47,17 +47,11 @@ class regression_simulation:
     """
 
     def __init__(self, X_train, X_predict, Y_train):
-        # won't this break ordering between Y and x?
-        # see @ljwolf's implementation later, in the
-        # regression coefficient example
         allometric_model = sm.GLM(
             Y_train,
             np.log(X_train+1),
             family=sm.families.Gamma(link=sm.families.links.Log()),
         )
-        # self.model = sm.GLM(Y_train, X_train, family=sm.families.Gaussian()) # sm.OLS(Y, X)
-        # print(X_train)
-
         self._fit_function = lambda y, x: (
             sm.GLM(
                 y, np.log(x+1), family=sm.families.Gamma(link=sm.families.links.Log())
@@ -183,7 +177,12 @@ class regression_simulation:
         prediction values at each inputted X_predict.
         """
         if X_predict is None:
-            X_predict = np.atleast_2d(self.X_predict).reshape(-1, 1)
+            if isinstance(self.X_predict, pd.DataFrame):
+                ncol_X_pred = len(self.X_predict.columns)
+            else:
+                ncol_X_pred = 1
+            X_predict = np.atleast_2d(self.X_predict).reshape(-1, ncol_X_pred)
+
         if conditional:
             assert boot is False, "cannot bootstrap conditional predictions"
             if params is None:
@@ -205,7 +204,10 @@ class regression_simulation:
             params = self.simulate_regression_params(
                 n_replicates=n_replicates, boot=boot
             )
-            lin_preds = X_predict @ params
+#            lin_preds = X_predict @ params.T
+
+            lin_preds = X_predict @ params.T
+
             simulated_data = np.column_stack(
                 [self.results.model.family.fitted(lin_pred) for lin_pred in lin_preds.T]
             )
@@ -233,19 +235,23 @@ class regression_simulation:
         )
         print("bias: " + str(bias))
 
-    def plot_fit_POC(self, boot=False, n_replicates=1, figsize=(12, 8)):
-        y = self.Y_train
-        x = self.X_train
+    def plot_fit_POC(self, x, y, boot=False, n_replicates=1, figsize=(12, 8)):
+        #y = self.Y_train
+        #x = self.X_train
 
         m = regression_simulation(x, x, y)
-        yhat = m.simulate_predictions(boot=boot, n_replicates=n_replicates).flatten()
+        yhat = pd.DataFrame({'yhat': m.simulate_predictions(
+                                                    boot=boot, 
+                                                    n_replicates=n_replicates).flatten()})
+
+        d = pd.concat([x, y, yhat], axis=1)
 
         fig, axs = plt.subplots(1, 2, figsize=figsize)
 
         axs[0].scatter(x, y)
-        sns.regplot(x=x, y=y, order=1, ax=axs[0], ci=None, scatter=False)
+        sns.regplot(x=d['volume'], y=d['pg poc'], order=1, ax=axs[0], ci=None, scatter=False)
 
-        sns.regplot(x=yhat, y=y, order=1, ax=axs[1], ci=None)
+        sns.regplot(x=d['yhat'], y=d['pg poc'], order=1, ax=axs[1], ci=None)
         abline_plot(slope=1, intercept=0, ax=axs[1], color="black", linestyle="dashed")
 
         axs[0].set_title("Allometric scaling")
@@ -284,83 +290,75 @@ class regression_simulation:
         print("intercept: " + str(intercept))
         print("r_value: " + str(r_value))
 
-    def plot_fit_PIC(self, figsize=(8, 8)):
+    def plot_fit_PIC(self, figsize=(8, 8), title=None, log_trans=True, boot=False, n_replicates=1):
         y = self.Y_train
-        x = self.X_train["Volume"]
+        x = self.X_train #["Volume"]
+        m = regression_simulation(x, x, y)
+        yhat = pd.DataFrame({'yhat': m.simulate_predictions(
+                                                    boot=boot, 
+                                                    n_replicates=n_replicates).flatten()})
 
-        yhat = self.results.mu
-        # log transform
-        y = np.log10(y)
-        yhat = np.log10(yhat)
-        x = np.log10(x)
-
-        y_HOL = y[self.X_train["Phase_HOL"] == 1]
-        yhat_HOL = yhat[self.X_train["Phase_HOL"] == 1]
-
-        y_HET = y[self.X_train["Phase_HET"] == 1]
-        yhat_HET = yhat[self.X_train["Phase_HET"] == 1]
-
-        x_HET = x[self.X_train["Phase_HET"] == 1]
-        x_HOL = x[self.X_train["Phase_HOL"] == 1]
-
-        HOL_line_fit = sm.OLS(y_HOL, yhat_HOL).fit()
-        HET_line_fit = sm.OLS(y_HET, yhat_HET).fit()
+        d = pd.concat([x, y, yhat], axis=1)
+        d.dropna(inplace=True)
+        #print(d)
 
         fig, axs = plt.subplots(2, 2, figsize=figsize)
 
-        # scaling HET
-        axs[0, 0].scatter(x_HET, y_HET, color="firebrick")
-        sns.regplot(
-            x=x_HET,
-            y=y_HET,
-            order=1,
-            ax=axs[0, 0],
-            color="darkred",
-            ci=None,
-            scatter=False,
+        sns.scatterplot(
+            data = d[d['Phase_HET']==1],
+            x='Volume',
+            y='PIC pg C',
+            ax = axs[0,0]
         )
+
+        sns.scatterplot(
+            data = d[d['Phase_HET']==1],
+            y='PIC pg C',
+            x='yhat',
+            ax = axs[0,1]
+        )
+
+        sns.scatterplot(
+            data = d[d['Phase_HOL']==1],
+            x='Volume',
+            y='PIC pg C',
+            ax = axs[1,0]
+        )
+
+        sns.scatterplot(
+            data = d[d['Phase_HOL']==1],
+            y='PIC pg C',
+            x='yhat',
+            ax = axs[1,1]
+        )
+        axs[0,1].plot(d[d['Phase_HET']==1]['PIC pg C'], d[d['Phase_HET']==1]['PIC pg C'])
+        axs[1,1].plot(d[d['Phase_HOL']==1]['PIC pg C'], d[d['Phase_HOL']==1]['PIC pg C'])
+
+        if log_trans==True:
+            axs[0,0].set_yscale('log')
+            axs[0,0].set_xscale('log')
+            axs[0,1].set_yscale('log')
+            axs[0,1].set_xscale('log')
+            axs[1,0].set_yscale('log')
+            axs[1,0].set_xscale('log')
+            axs[1,1].set_yscale('log')
+            axs[1,1].set_xscale('log')
+
         axs[0, 0].set_title("Scaling (diploid)")
         axs[0, 0].set_ylabel("C content (pg C)")
         axs[0, 0].set_xlabel("Cell size (um3)")
 
-        # Fit HET
-        axs[0, 1].scatter(yhat_HET, y_HET, color="firebrick")
-        abline_plot(model_results=HET_line_fit, ax=axs[0, 1], color="darkred")
-        abline_plot(
-            slope=1, intercept=0, ax=axs[0, 1], color="black", linestyle="dashed"
-        )
         axs[0, 1].set_title("Fit (diploid)")
         axs[0, 1].set_ylabel("Observed (pg C)")
         axs[0, 1].set_xlabel("Predicted (pg C)")
-        axs[0, 1].set_xlim(0, np.nanmax([y, yhat]))
-        axs[0, 1].set_ylim(0, np.nanmax([y, yhat]))
 
-        # scaling HOL
-        axs[1, 0].scatter(x_HOL, y_HOL, color="steelblue")
-        sns.regplot(
-            x=x_HOL,
-            y=y_HOL,
-            order=1,
-            ax=axs[1, 0],
-            color="darkblue",
-            ci=None,
-            scatter=False,
-        )
         axs[1, 0].set_title("Scaling (haploid)")
         axs[1, 0].set_ylabel("C content (pg C)")
         axs[1, 0].set_xlabel("Cell size (um3)")
 
-        # Fit HOL
-        axs[1, 1].scatter(yhat_HOL, y_HOL, color="steelblue")
-        abline_plot(model_results=HOL_line_fit, ax=axs[1, 1], color="darkblue")
-        abline_plot(
-            slope=1, intercept=0, ax=axs[1, 1], color="black", linestyle="dashed"
-        )
         axs[1, 1].set_title("Fit (haploid)")
         axs[1, 1].set_ylabel("Observed (pg C)")
         axs[1, 1].set_xlabel("Predicted (pg C)")
-        axs[1, 1].set_xlim(0, np.nanmax([y, yhat]))
-        axs[1, 1].set_ylim(0, np.nanmax([y, yhat]))
 
         # axs[1,0].set_aspect('equal', adjustable='box')
         # axs[1,1].set_aspect('equal', adjustable='box')
@@ -404,93 +402,186 @@ class regression_simulation:
             va="top",
         )
 
-        fig.suptitle("Allometric GLM for PIC", size=24, weight="bold")
+        # fig.suptitle("Allometric GLM for PIC", size=24, weight="bold")
+        # # axs[1,1].set_xscale("log")
+        # # axs[0,1].set_xscale("log")
+        # # axs[1,1].set_yscale("log")
+        # # axs[0,1].set_yscale("log")
 
+        # # axs[1,0].set_xscale("log")
+        # # axs[0,0].set_xscale("log")
+        # # axs[1,0].set_yscale("log")
+        # # axs[0,0].set_yscale("log")
+        fig.suptitle(title)
         plt.tight_layout()
         plt.show()
 
 
 if __name__ == "__main__":
-    d = pd.read_csv("../data/unprocessed/poulton2024.csv")
-    Y_train = d["pg poc"]
-    X_train = d["volume"]
-    X_predict = np.random.normal(8, 2, 1000)
 
-    loggamma_raw_x = sm.GLM(
-        Y_train, X_train, family=sm.families.Gamma(link=sm.families.links.Log())
-    ).fit()
-    loggamma = sm.GLM(
-        Y_train,
-        np.log(X_train),
-        family=sm.families.Gamma(link=sm.families.links.Log()),
-    ).fit()
-    ident_gamma = sm.GLM(
-        Y_train, X_train, family=sm.families.Gamma(link=sm.families.links.Identity())
-    ).fit()
-    loglinear = sm.OLS(np.log(Y_train), np.log(X_train)).fit()
+    test_POC = True
+    test_PIC = True
+    plot_PIC = True
 
-    support = np.linspace(X_train.min(), X_train.max())
 
-    preds_gamma_raw_x = loggamma_raw_x.predict(support)
-    preds_log_gamma = loggamma.predict(np.log(support))
-    preds_ident_gamma = ident_gamma.predict(support)
-    preds_loglinear = np.exp(loglinear.predict(np.log(support)))
+    if test_POC == True:
+        print("testing POC")
+        #    d = pd.read_csv("../data/unprocessed/poulton2024.csv")
+        d = pd.read_csv("/home/phyto/CoccoData/data/unprocessed/poulton2024.csv")
+        Y_train = d['pg poc']
+        X_train = d['volume']
 
-    f, ax = plt.subplots(1, 1)
-    ax.scatter(d["volume"], d["pg poc"], color="k")
-    for pred_type, preds in [
-        (k, v) for k, v in locals().items() if k.startswith("preds_")
-    ]:
-        if pred_type.endswith("raw_x"):
-            continue
-        ax.plot(
-            support, preds, label=pred_type.lstrip("preds_").replace("_", " ").title()
-        )
-    ax.legend()
-    ax.set_yscale("log")
-    ax.set_xscale("log")
-    plt.draw()
+        def diameter_to_volume(d):
+            v = (1/6) * np.pi * d**3
+            return(v)
 
-    m = regression_simulation(X_train, X_predict, Y_train)
+        v = diameter_to_volume(10)
+        X_predict = np.random.normal(v, v*0.2, 1000)
+        X_predict = X_predict[X_predict>0]
+        m = regression_simulation(X_train, X_predict, Y_train)
 
-    normal_approx = m.simulate_regression_params()
-    booted_slopes = m.simulate_regression_params(boot=True)
-    plot_data = pd.concat(
-        (
-            pd.DataFrame(normal_approx, columns=["slope estimate"]).assign(
-                style="mle asymptotics"
-            ),
-            pd.DataFrame(booted_slopes, columns=["slope estimate"]).assign(
-                style="boostrapped"
-            ),
-        )
-    )
-    f, ax = plt.subplots(1, 1, figsize=(6, 4))
-    sns.kdeplot(plot_data, ax=ax, hue="style", x="slope estimate")
-    plt.draw()
 
-    conditional = m.simulate_predictions()
-    unconditional = m.simulate_predictions(conditional=False)
-    unconditional_booted = m.simulate_predictions(boot=True, conditional=False)
+        poc = m.simulate_predictions(boot=True, n_replicates=1)
+        median = np.percentile(poc, 50)
+        print("boot = True:")
+        print(median)
+        print("expected value ~70")
+        m.plot_fit_POC(X_train, Y_train, boot=True, n_replicates=1)
 
-    plot_data = pd.concat(
-        (
-            pd.DataFrame(conditional, columns=["conditional"]),
-            pd.DataFrame(unconditional, columns=["unconditional"]),
-            pd.DataFrame(unconditional_booted, columns=["unconditional_booted"]),
-        ),
-        axis=1,
-    ).melt(var_name="simulation_type", value_name="replicate")
-    ax = sns.kdeplot(data=plot_data, hue="simulation_type", x="replicate")
-    ax.set_xscale("log")
-    plt.show()
 
-    # test1 = m.simulate_data()
-    # sns.histplot(x=test1)
+        poc = m.simulate_predictions(boot=False, n_replicates=1)
+        median = np.percentile(poc, 50)
+        print("boot = False:")
+        print(median)
+        print("expected value ~70")
+        m.plot_fit_POC(X_train, Y_train, boot=False, n_replicates=1)
+
+
+    if test_PIC == True:        
+        print("testing PIC")
+
+        #d = pd.read_csv("../data/unprocessed/rosie_size_pic.csv")
+        d = pd.read_csv("/home/phyto/CoccoData/data/unprocessed/rosie_size_pic.csv")
+        d = d.dropna()
+        d = d[d['PIC pg C'] >0]
+        d = d[d['Volume'] >0]
+        Y_train = d['PIC pg C']
+
+        def diameter_to_volume(d):
+            v = (1/6) * np.pi * d**3
+            return(v)
+
+        v = diameter_to_volume(10)
+        
+        d = pd.get_dummies(d, columns=['Phase'], dtype=float)
+        X_train = d[['Volume', 'Phase_HET', 'Phase_HOL']].astype(float)
+
+        X_predict = pd.DataFrame({'Volume': np.random.normal(v, v*0.2, 1000), 
+                            'Phase_HET':[1]*1000, 
+                            'Phase_HOL':[0]*1000})
+        X_predict= X_predict[X_predict['Volume']>0]
+
+        m = regression_simulation(X_train, X_predict, Y_train)
+
+        pic = m.simulate_predictions(boot=False, n_replicates=1)
+        median = np.percentile(pic, 50)
+        print("boot = False:")
+        print(median)
+        print("expected value ~20")
+        m.plot_fit_PIC(title="conditional", boot=False, n_replicates=1)
+        #m.return_performance()
+
+        #m.return_performance()
+        #if plot_PIC == True:
+        #    sns.histplot(x=pic.flatten())
+        #plt.show()
+
+        m.plot_fit_PIC(title="boot", boot=True, n_replicates=1)
+        pic = m.simulate_predictions(boot=True, n_replicates=1)
+        median = np.percentile(pic, 50)
+        print("boot = True:")
+        print(median)
+        print("expected value ~20")
+        #m.return_performance()
+
+
+
+
+
+    # d = pd.read_csv("../data/unprocessed/poulton2024.csv")
+    # Y_train = d["pg poc"]
+    # X_train = d["volume"]
+    # X_predict = np.random.normal(8, 2, 1000)
+
+    # loggamma_raw_x = sm.GLM(
+    #     Y_train, X_train, family=sm.families.Gamma(link=sm.families.links.Log())
+    # ).fit()
+    # loggamma = sm.GLM(
+    #     Y_train,
+    #     np.log(X_train),
+    #     family=sm.families.Gamma(link=sm.families.links.Log()),
+    # ).fit()
+    # ident_gamma = sm.GLM(
+    #     Y_train, X_train, family=sm.families.Gamma(link=sm.families.links.Identity())
+    # ).fit()
+    # loglinear = sm.OLS(np.log(Y_train), np.log(X_train)).fit()
+
+    # support = np.linspace(X_train.min(), X_train.max())
+
+    # preds_gamma_raw_x = loggamma_raw_x.predict(support)
+    # preds_log_gamma = loggamma.predict(np.log(support))
+    # preds_ident_gamma = ident_gamma.predict(support)
+    # preds_loglinear = np.exp(loglinear.predict(np.log(support)))
+
+    # f, ax = plt.subplots(1, 1)
+    # ax.scatter(d["volume"], d["pg poc"], color="k")
+    # for pred_type, preds in [
+    #     (k, v) for k, v in locals().items() if k.startswith("preds_")
+    # ]:
+    #     if pred_type.endswith("raw_x"):
+    #         continue
+    #     ax.plot(
+    #         support, preds, label=pred_type.lstrip("preds_").replace("_", " ").title()
+    #     )
+    # ax.legend()
+    # ax.set_yscale("log")
+    # ax.set_xscale("log")
+    # plt.draw()
+
+    # m = regression_simulation(X_train, X_predict, Y_train)
+
+    # normal_approx = m.simulate_regression_params()
+    # booted_slopes = m.simulate_regression_params(boot=True)
+    # plot_data = pd.concat(
+    #     (
+    #         pd.DataFrame(normal_approx, columns=["slope estimate"]).assign(
+    #             style="mle asymptotics"
+    #         ),
+    #         pd.DataFrame(booted_slopes, columns=["slope estimate"]).assign(
+    #             style="boostrapped"
+    #         ),
+    #     )
+    # )
+    # f, ax = plt.subplots(1, 1, figsize=(6, 4))
+    # sns.kdeplot(plot_data, ax=ax, hue="style", x="slope estimate")
+    # plt.draw()
+
+    # conditional = m.simulate_predictions()
+    # unconditional = m.simulate_predictions(conditional=False)
+    # unconditional_booted = m.simulate_predictions(boot=True, conditional=False)
+
+    # plot_data = pd.concat(
+    #     (
+    #         pd.DataFrame(conditional, columns=["conditional"]),
+    #         pd.DataFrame(unconditional, columns=["unconditional"]),
+    #         pd.DataFrame(unconditional_booted, columns=["unconditional_booted"]),
+    #     ),
+    #     axis=1,
+    # ).melt(var_name="simulation_type", value_name="replicate")
+    # ax = sns.kdeplot(data=plot_data, hue="simulation_type", x="replicate")
+    # ax.set_xscale("log")
     # plt.show()
-    # m.return_performance()
 
-    # m.plot_fit_POC()
 
 
 # d = pd.read_csv("/home/phyto/CoccoData/data/unprocessed/poulton2024.csv")
